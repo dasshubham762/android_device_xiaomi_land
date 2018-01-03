e9f656f71d235c1f24889a3293535c3b21b5de41
Shubham Das <dasshubham762@gmail.com>
2018-01-03 18:33:36 +0530

    land: update sepolicies
    * Remove qcom-sepolicy
    
    Signed-off-by: Shubham Das <dasshubham762@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,6 @@ USE_SENSOR_MULTI_HAL := true
 # SELinux
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
-include device/qcom/sepolicy/sepolicy.mk
 
 # Time
 BOARD_USES_QC_TIME_SERVICES := true
